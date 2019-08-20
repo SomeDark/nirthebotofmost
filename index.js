9ec69f183f3aeefa3b8bb4178cee72699dbede89
SomeDark <54318509+SomeDark@users.noreply.github.com>
2019-08-20 17:33:07 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,41 @@
+const Discord = require('discord.js');;
+const bot = new Discord.Client();
+
+const token = 'NjEzMzc5MjAxMzE5MzA1MjM2.XVwECQ.eMkvcnJGeIrfxl7ACfBlJ2WFz4k';
+
+bot.on('ready', () =>{
+    console.log('this bot is online')
+})
+
+
+
+bot.on('message', msg=>{
+if(msg.content === "מה קורה בוט"){
+msg.reply('אתה יודע קשה להיות בוט...');
+}
+})
+
+bot.on('message', msg=>{
+    if(msg.content === "בוט מטומטם"){
+    msg.reply('נפגעתי תפסיק לפגוע לי ברגשות');
+    }
+    })
+
+    bot.on('message', msg=>{
+        if(msg.content === "מה התחביבים שלך בוט?"){
+        msg.reply('לגעת בטל ולהטריד אותו');
+        }
+        })
+
+        bot.on('message', msg=>{
+            if(msg.content === "בוט אני יכול לקבל נשיקה בפופיק"){
+            msg.reply('ברורררר');
+            }
+            })
+
+            bot.on('message', msg=>{
+                if(msg.content === "בוט מה אתה חושב על שליו צור"){
+                msg.reply('שליו צור המלך');
+                }
+                })
+bot.login(token);
